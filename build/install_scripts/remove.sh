26c4fc7de5a40e54a9d40fe8a1d476b1cafa51f0
zendrique <40753927+zendrique@users.noreply.github.com>
2020-05-08 18:00:12 +0200

    Delete remove.sh

--- build/install_scripts/remove.sh
+++
@@ -1,8 +0,0 @@
-#Supression de mc-scipt
-echo "Suppression du script..."
-echo "Script par zendrique https://github.com/zendrique"
-echo "Contributeur Yadasko https://github.com/Yadasko"
-cd /home
-rm -f script.sh
-echo "Fermture..."
-exit
