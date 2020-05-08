436324d84e0d213af92b08773a6249779bd07252
zendrique <40753927+zendrique@users.noreply.github.com>
2020-05-08 18:00:44 +0200

    Delete system_info.sh

--- build/install_scripts/system_info.sh
+++
@@ -1,8 +0,0 @@
-
-# Info système
-clear
-echo "Votre Système :"
-uname -a
-echo "Contributeurs : Zendrique et Yadasko"
-echo "Se script est concue pour fonctionner avec les systémes d'exploitation utilisant comment gestionnaire de packet apt"
-echo "Sous recommendons d'utiliser debian ou ubuntu."
