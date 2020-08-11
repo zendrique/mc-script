13ab414bbd75c17ab31329c62d12012bd830f85c
zendrique <40753927+zendrique@users.noreply.github.com>
2020-08-11 18:25:45 +0200

    Update vanilla.sh

--- build/modules/vanilla.sh
+++ build/modules/vanilla.sh
@@ -1,7 +1,7 @@
 # Vanilla
 
-version_vanilla=1.16.1
-vanilla_download=https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar
+version_vanilla=1.16.2
+vanilla_download=https://launcher.mojang.com/v1/objects/c5f6fb23c3876461d46ec380421e42b289789530/server.jar
 
 clear
 echo "Installation du serveur : Vanilla"
