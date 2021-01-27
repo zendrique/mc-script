fa88f75c8dacdc3a40e58285d72208d1a4799619
zendrique <40753927+zendrique@users.noreply.github.com>
2021-01-27 21:28:44 +0100

    Update forge.sh

--- build/modules/forge.sh
+++ build/modules/forge.sh
@@ -22,7 +22,7 @@ select sauvegarde in "Oui" "Non"; do
 done
 }
 
-function 1.16.5 {
+function installation {
     echo "Instalation de : Forge" $version
     cd /home
     echo "Dans quel dossier voulez-vous installer votre serveur ? (ex: serveur1)"
@@ -39,335 +39,39 @@ function 1.16.5 {
     rm forge.jar
     rm start.sh
     rm eula.txt
-    rm -R libraries
-    mkdir mods
-    chmod 777 mods/
-    chmod 777 -R mods/
-    clear
-    echo "Téléchargement de forge"
-    wget $forge_16_download
-    mv forge-*-installer.jar forge-installer.jar
-    clear
-    echo "Installation de forge" $version
-    java -jar forge-installer.jar --installServer
-    rm -f forge-installer.jar
-    mv forge-*.jar forge.jar
-    touch eula.txt && echo "eula=true" >> eula.txt
-    touch start.sh && echo "cd /home/"$dossier"" >> start.sh
-    echo "screen -d -m -S "$dossier" java -jar forge.jar nogui" >> start.sh
-    chmod 777 start.sh
-    chmod 777 forge.jar
-    echo "Nettoyage..."
-    rm installer.log
-    rm forge-installer.jar.log
-    cd /home
-    chmod 777 -R $dossier/
-    rm info-$dossier.txt
-    touch info-$dossier.txt
-    echo "Pour démarer votre serveur faites la commande : bash /home/"$dossier"/start.sh" >> info-$dossier.txt
-    echo "Pour accéder à votre console taper la commande : screen -r" $dossier >> info-$dossier.txt
-    echo "Detail de l'installation :" >> info-$dossier.txt
-    echo "Version du serveur :" $version >> info-$dossier.txt
-    echo "API : Forge" >> info-$dossier.txt
-    echo "Dossier d'instalation : /home/"$dossier >> info-$dossier.txt
-    echo "Dossier des mods : /home/"$dossier"/mods" >> info-$dossier.txt
-    echo "Fichier de démarrage de : start.sh" >> info-$dossier.txt
-    clear
-    echo "Terminer !"
-    echo "Pour démarer votre serveur faites la commande : bash /home/"$dossier"/start.sh"
-    echo "Pour accéder à votre console taper la commande : screen -r" $dossier
-    echo "Detail de l'installation :"
-    echo "Version du serveur : 1.16.5"
-    echo "API : Forge"
-    echo "Dossier d'instalation : /home/"$dossier
-    echo "Dossier des mods : /home/"$dossier"/mods"
-    echo "Fichier de démarrage de : start.sh"
-    echo "Un fichier info-"$dossier".txt dans /home a été crée contenant les information afficher si dessue."
-    exit
-}
-
-function 1.15.2 {
-    clear
-    echo "Instalation de : Forge" $version
-    cd /home
-    echo "Dans quel dossier voulez-vous installer votre serveur ? (ex: serveur1)"
-    read dossier
-    backup
-    rm $dossier-backup.zip
-    zip -r $dossier-backup.zip $dossier/
-    clear
-    mkdir $dossier
-    chmod 777 $dossier/
-    chmod 777 -R $dossier/
-    cd $dossier
-    rm forge-*.jar
-    rm forge.jar
-    rm start.sh
-    rm eula.txt
-    rm -R libraries
-    mkdir mods
-    chmod 777 mods/
-    chmod 777 -R mods/
-    clear
-    echo "Téléchargement de forge"
-    wget $forge_15_download
-    mv forge-*-installer.jar forge-installer.jar
-    clear
-    echo "Installation de forge" $version
-    java -jar forge-installer.jar --installServer
-    rm forge-installer.jar
-    mv forge-*.jar forge.jar
-    touch eula.txt && echo "eula=true" >> eula.txt
-    touch start.sh && echo "cd /home/"$dossier"" >> start.sh
-    echo "screen -d -m -S "$dossier" java -jar forge.jar nogui" >> start.sh
-    chmod 777 start.sh
-    chmod 777 forge.jar
-    echo "Nettoyage..."
-    rm installer.log
-    rm forge-installer.jar.log
-    cd /home
-    chmod 777 -R $dossier/
-    rm info-$dossier.txt
-    touch info-$dossier.txt
-    echo "Pour démarer votre serveur faites la commande : bash /home/"$dossier"/start.sh" >> info-$dossier.txt
-    echo "Pour accéder à votre console taper la commande : screen -r" $dossier >> info-$dossier.txt
-    echo "Detail de l'installation :" >> info-$dossier.txt
-    echo "Version du serveur :" $version >> info-$dossier.txt
-    echo "API : Forge" >> info-$dossier.txt
-    echo "Dossier d'instalation : /home/"$dossier >> info-$dossier.txt
-    echo "Dossier des mods : /home/"$dossier"/mods" >> info-$dossier.txt
-    echo "Fichier de démarrage de : start.sh" >> info-$dossier.txt
-    clear
-    echo "Terminer !"
-    echo "Pour démarer votre serveur faites la commande : bash /home/"$dossier"/start.sh"
-    echo "Pour accéder à votre console taper la commande : screen -r" $dossier
-    echo "Detail de l'installation :"
-    echo "Version du serveur :" $version
-    echo "API : Forge"
-    echo "Dossier d'instalation : /home/"$dossier
-    echo "Dossier des mods : /home/"$dossier"/mods"
-    echo "Fichier de démarrage de : start.sh"
-    echo "Un fichier info-"$dossier".txt dans /home a été crée contenant les information afficher si dessue."
-    exit
-}
-
-function 1.12.2 {
-    clear
-    echo "Instalation de : Forge" $version
-    cd /home
-    echo "Dans quel dossier voulez-vous installer votre serveur ? (ex: serveur1)"
-    read dossier
-    backup
-    rm $dossier-backup.zip
-    zip -r $dossier-backup.zip $dossier/
-    clear
-    mkdir $dossier
-    chmod 777 $dossier/
-    chmod 777 -R $dossier/
-    cd $dossier
-    rm start.sh
-    rm eula.txt
-    rm forge.jar
-    rm forge-*.jar
-    rm -R libraries
-    mkdir mods
-    chmod 777 mods/
-    chmod 777 -R mods/
-    clear
-    echo "Téléchargement de forge"
-    wget $forge_12_download
-    mv forge-*-installer.jar forge-installer.jar
-    clear
-    echo "Installation de forge" $version
-    java -jar forge-installer.jar --installServer
-    rm -f forge-installer.jar
-    mv forge-*.jar forge.jar
-    touch eula.txt && echo "eula=true" >> eula.txt
-    touch start.sh && echo "cd /home/"$dossier"" >> start.sh
-    echo "screen -d -m -S "$dossier" java -jar forge.jar nogui" >> start.sh
-    chmod 777 start.sh
-    chmod 777 forge.jar
-    echo "Nettoyage..."
-    rm installer.log
-    rm forge-installer.jar.log
-    cd /home
-    chmod 777 -R $dossier/
-    rm -f info-$dossier.txt
-    touch info-$dossier.txt
-    echo "Pour démarer votre serveur faites la commande : bash /home/"$dossier"/start.sh" >> info-$dossier.txt
-    echo "Pour accéder à votre console taper la commande : screen -r" $dossier >> info-$dossier.txt
-    echo "Detail de l'installation" >> info-$dossier.txt
-    echo "Version du serveur :" $version >> info-$dossier.txt
-    echo "API : Forge" >> info-$dossier.txt
-    echo "Dossier d'instalation : /home/"$dossier >> info-$dossier.txt
-    echo "Fichier de démarrage de : start.sh" >> info-$dossier.txt
-    clear
-    echo "Terminer !"
-    echo "Pour démarer votre serveur faites la commande : bash /home/"$dossier"/start.sh"
-    echo "Pour accéder à votre console taper la commande : screen -r" $dossier
-    echo "Detail de l'installation"
-    echo "Version du serveur :" $version
-    echo "API : Forge"
-    echo "Dossier d'instalation : /home/"$dossier
-    echo "Fichier de démarrage de : start.sh"
-    echo "Un fichier info-"$dossier".txt dans /home a été crée contenant les information afficher si dessue."
-    exit
-}
-
-function 1.10.2 {
-    clear
-    echo "Instalation de : Forge" $version
-    cd /home
-    echo "Dans quel dossier voulez-vous installer votre serveur ? (ex: serveur1)"
-    read dossier
-    backup
-    rm $dossier-backup.zip
-    zip -r $dossier-backup.zip $dossier/
-    clear
-    mkdir $dossier
-    chmod 777 $dossier/
-    chmod 777 -R $dossier/
-    cd $dossier
-    rm start.sh
-    rm eula.txt
-    rm forge.jar
-    rm forge-*.jar
-    rm -R libraries
-    mkdir mods
-    chmod 777 mods/
-    chmod 777 -R mods/
-    clear
-    echo "Téléchargement de forge"
-    wget $forge_10_download
-    mv forge-*-installer.jar forge-installer.jar
-    clear
-    echo "Installation de forge" $version
-    java -jar forge-installer.jar --installServer
-    rm -f forge-installer.jar
-    mv forge-*.jar forge.jar
-    touch eula.txt && echo "eula=true" >> eula.txt
-    touch start.sh && echo "cd /home/"$dossier"" >> start.sh
-    echo "screen -d -m -S "$dossier" java -jar forge.jar nogui" >> start.sh
-    chmod 777 start.sh
-    chmod 777 forge.jar
-    echo "Nettoyage..."
-    rm installer.log
-    rm forge-installer.jar.log
-    cd /home
-    chmod 777 -R $dossier/
-    rm -f info-$dossier.txt
-    touch info-$dossier.txt
-    echo "Pour démarer votre serveur faites la commande : bash /home/"$dossier"/start.sh" >> info-$dossier.txt
-    echo "Pour accéder à votre console taper la commande : screen -r" $dossier >> info-$dossier.txt
-    echo "Detail de l'installation" >> info-$dossier.txt
-    echo "Version du serveur :" $version >> info-$dossier.txt
-    echo "API : Forge" >> info-$dossier.txt
-    echo "Dossier d'instalation : /home/"$dossier >> info-$dossier.txt
-    echo "Fichier de démarrage de : start.sh" >> info-$dossier.txt
-    clear
-    echo "Terminer !"
-    echo "Pour démarer votre serveur faites la commande : bash /home/"$dossier"/start.sh"
-    echo "Pour accéder à votre console taper la commande : screen -r" $dossier
-    echo "Detail de l'installation"
-    echo "Version du serveur :" $version
-    echo "API : Forge"
-    echo "Dossier d'instalation : /home/"$dossier
-    echo "Fichier de démarrage de : start.sh"
-    echo "Un fichier info-"$dossier".txt dans /home a été crée contenant les information afficher si dessue."
-    exit
-}
-
-function 1.7.10 {
-    echo "Instalation de : Forge" $version
-    cd /home
-    echo "Dans quel dossier voulez-vous installer votre serveur ? (ex: serveur1)"
-    read dossier
-    backup
-    rm $dossier-backup.zip
-    zip -r $dossier-backup.zip $dossier/
-    clear
-    mkdir $dossier
-    chmod 777 $dossier/
-    chmod 777 -R $dossier/
-    cd $dossier
-    rm start.sh
-    rm eula.txt
-    rm forge.jar
-    rm forge-*.jar
     rm -R libraries
     mkdir mods
     chmod 777 mods/
     chmod 777 -R mods/
     clear
     echo "Téléchargement de forge" $version
-    wget $forge_7_download
+    if [ "$version" == "1.16.5" ]
+    then
+      wget $forge_16_download
+    fi
+    if [ "$version" == "1.15.2" ]
+    then
+      wget $forge_15_download
+    fi
+    if [ "$version" == "1.12.2" ]
+    then
+      wget $forge_12_download
+    fi
+    if [ "$version" == "1.10.2" ]
+    then
+      wget $forge_10_download
+    fi
+    if [ "$version" == "1.7.10" ]
+    then
+      wget $forge_7_download
+    fi
+    if [ "$version" == "autre" ]
+    then
+      wget $lien
+    fi
     mv forge-*-installer.jar forge-installer.jar
     clear
     echo "Installation de forge" $version
-    java -jar forge-installer.jar --installServer
-    rm forge-installer.jar
-    mv forge-*.jar forge.jar
-    touch eula.txt && echo "eula=true" >> eula.txt
-    touch start.sh && echo "cd /home/"$dossier"" >> start.sh
-    echo "screen -d -m -S "$dossier" java -jar forge.jar nogui" >> start.sh
-    chmod 777 start.sh
-    chmod 777 forge.jar
-    echo "Nettoyage..."
-    rm installer.log
-    rm forge-installer.jar.log
-    cd /home
-    chmod 777 -R $dossier/
-    rm -f info-$dossier.txt
-    touch info-$dossier.txt
-    echo "Pour démarer votre serveur faites la commande : bash /home/"$dossier"/start.sh" >> info-$dossier.txt
-    echo "Pour accéder à votre console taper la commande : screen -r" $dossier >> info-$dossier.txt
-    echo "Detail de l'installation" >> info-$dossier.txt
-    echo "Version du serveur :" $version >> info-$dossier.txt
-    echo "API : Forge" >> info-$dossier.txt
-    echo "Dossier d'instalation : /home/"$dossier >> info-$dossier.txt
-    echo "Fichier de démarrage de : start.sh" >> info-$dossier.txt
-    clear
-    echo "Terminer !"
-    echo "Pour démarer votre serveur faites la commande : bash /home/"$dossier"/start.sh"
-    echo "Pour accéder à votre console taper la commande : screen -r" $dossier
-    echo "Detail de l'installation"
-    echo "Version du serveur :" $version
-    echo "API : Forge"
-    echo "Dossier d'instalation : /home/"$dossier
-    echo "Fichier de démarrage de : start.sh"
-    echo "Un fichier info-"$dossier".txt dans /home a été crée contenant les information afficher si dessue."
-    exit
-}
-
-function autre {
-    clear
-    echo "Instalation de : Une autre version de forge"
-    cd /home
-    echo "Dans quel dossier voulez-vous installer votre serveur ? (ex: serveur1)"
-    read dossier
-    backup
-    rm $dossier-backup.zip
-    zip -r $dossier-backup.zip $dossier/
-    clear
-    mkdir $dossier
-    chmod 777 $dossier/
-    chmod 777 -R $dossier/
-    cd $dossier
-    rm start.sh
-    rm eula.txt
-    rm forge.jar
-    rm forge-*.jar
-    rm -R libraries
-    mkdir mods
-    chmod 777 mods/
-    chmod 777 -R mods/
-    clear
-    echo "Merci d'indiquer le lien de tléchargement direct de l'installateur non windows de forge."
-    read lien
-    wget $lien
-    mv forge-*-installer.jar forge-installer.jar
-    clear
-    echo "Installation de forge"
     java -jar forge-installer.jar --installServer
     rm -f forge-installer.jar
     mv forge-*.jar forge.jar
@@ -381,23 +85,25 @@ function autre {
     rm forge-installer.jar.log
     cd /home
     chmod 777 -R $dossier/
-    rm -f info-$dossier.txt
+    rm info-$dossier.txt
     touch info-$dossier.txt
     echo "Pour démarer votre serveur faites la commande : bash /home/"$dossier"/start.sh" >> info-$dossier.txt
     echo "Pour accéder à votre console taper la commande : screen -r" $dossier >> info-$dossier.txt
-    echo "Detail de l'installation" >> info-$dossier.txt
-    echo "Version du serveur : Forge autre" >> info-$dossier.txt
+    echo "Detail de l'installation :" >> info-$dossier.txt
+    echo "Version du serveur :" $version >> info-$dossier.txt
     echo "API : Forge" >> info-$dossier.txt
     echo "Dossier d'instalation : /home/"$dossier >> info-$dossier.txt
+    echo "Dossier des mods : /home/"$dossier"/mods" >> info-$dossier.txt
     echo "Fichier de démarrage de : start.sh" >> info-$dossier.txt
     clear
     echo "Terminer !"
     echo "Pour démarer votre serveur faites la commande : bash /home/"$dossier"/start.sh"
     echo "Pour accéder à votre console taper la commande : screen -r" $dossier
-    echo "Detail de l'installation"
-    echo "Version du serveur : Forge autre"
+    echo "Detail de l'installation :"
+    echo "Version du serveur :" $version
     echo "API : Forge"
     echo "Dossier d'instalation : /home/"$dossier
+    echo "Dossier des mods : /home/"$dossier"/mods"
     echo "Fichier de démarrage de : start.sh"
     echo "Un fichier info-"$dossier".txt dans /home a été crée contenant les information afficher si dessue."
     exit
@@ -406,11 +112,13 @@ function autre {
 echo "Quel version de forge voulez-vous installer ? (1, 2, 3 uo 4)" 
 select version in "1.16.5" "1.15.2" "1.12.2" "1.10.2" "1.7.10" "autre"; do
     case $version in
-        1.16.5) 1.16.5 break;;
-        1.15.2 ) 1.15.2 break;;
-        1.12.2 ) 1.12.2 break;;
-        1.10.2 ) 1.10.2 break;;
-        1.7.10 ) 1.7.10 break;;
-        autre ) autre break;;
+        1.16.5) instalation break;;
+        1.15.2 ) instalation break;;
+        1.12.2 ) instalation break;;
+        1.10.2 ) instalation break;;
+        1.7.10 ) instalation break;;
+        autre ) echo "Merci d'indiquer le lien de téléchargement directe de l'installateur forge au format jar" 
+        read lien
+        instalation break;;
         esac
 done
