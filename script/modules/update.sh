9a062bf6c24ee777e556fbfd95774e2b613244fc
zendrique <40753927+zendrique@users.noreply.github.com>
2021-01-28 19:34:08 +0100

    Update update.sh

--- script/modules/update.sh
+++ script/modules/update.sh
@@ -5,5 +5,6 @@ apt update && cd /opt
 rm -R mc-script
 clear && apt install dos2unix git -y
 git clone https://github.com/zendrique/mc-script
+dos2unix /opt/mc-script/installation.sh
 bash /opt/mc-script/installation.sh --update
 exit
