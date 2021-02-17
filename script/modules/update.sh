10032bab315f4c5c8c57e62f19c2f60866d2fe8a
Zendrique <40753927+zendrique@users.noreply.github.com>
2021-02-17 21:06:05 +0100

    Update update.sh

--- script/modules/update.sh
+++ script/modules/update.sh
@@ -8,7 +8,7 @@ apt install dos2unix git -y
 clear
 echo "[.] Supression des fichiers..."
 rm -R $dossier/script/
-rm $dossier/LICENSE $dossier/README.md $dossier/boot.sh $dossier/initialisation.sh
+rm $dossier/README.md $dossier/boot.sh $dossier/initialisation.sh
 clear
 echo "[.] Téléchargement de mc-script..."
 mkdir $dossier/update
@@ -16,9 +16,8 @@ cd $dossier/update
 git clone https://github.com/zendrique/mc-script
 clear
 echo "[.] Préparation de la mise à jour..."
-mv $dossier_update/LICENSE $dossier
 mv $dossier_update/installation.sh $dossier
-mv $dossier_update/script $dossier
+mv $dossier_update/script/ $dossier
 dos2unix $dossier/installation.sh
 echo "[.] Démarrage du programme..."
 bash /opt/mc-script/installation.sh --update
