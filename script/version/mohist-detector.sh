c963e7af1300116db9a633e90d49a03dcba329eb
Zendrique <40753927+zendrique@users.noreply.github.com>
2021-03-16 19:25:27 +0100

    Update mohist-detector.sh

--- script/version/mohist-detector.sh
+++ script/version/mohist-detector.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
-pach="/opt/mc-script/variable"
+patch="/opt/mc-script/variable"
 
 if [ "$1" == "--1.7.10" ]
 then
   clear
   echo "Détermination du lien..."
   version_7=$(curl -s 'https://mohistmc.com/api/1.7.10/latest' | jq -r '.url')
-  echo $version_7 >> $pach/mohist-version.txt
+  echo $version_7 >> $patch/mohist-version.txt
 fi
 if [ "$1" == "--1.12.2" ]
 then
   clear
   echo "Détermination du lien..."
   version_12=$(curl -s 'https://mohistmc.com/api/1.12.2/latest' | jq -r '.url')
-  echo $version_12 >> $pach/mohist-version.txt
+  echo $version_12 >> $patch/mohist-version.txt
 fi
 if [ "$1" == "--1.16.5" ]
 then
   clear
   echo "Détermination du lien..."
   version_16=$(curl -s 'https://mohistmc.com/api/1.16.5/latest' | jq -r '.url')
-  echo $version_16 >> $pach/mohist-version.txt
+  echo $version_16 >> $patch/mohist-version.txt
 fi
 exit
